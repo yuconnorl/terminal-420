d443c10ee20e6ed2dd1c43c70b0ada51f66ad3d1
yuconnorl <yuconnorl@gmail.com>
2023-03-16 16:52:00 +0800

    feat: update metadata for pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,16 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'About',
+  description: 'About pages',
+}
+
 const AboutPage = () => {
   return (
     <div>
       <div>this is a about page</div>
     </div>
-  );
-};
+  )
+}
 
-export default AboutPage;
+export default AboutPage
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -6,8 +6,8 @@ import Link from 'next/link'
 import Footer from '@/components/Footer'
 
 export const metadata: Metadata = {
-  title: 'Blog posts',
-  description: 'blog postas',
+  title: 'Blog',
+  description: 'Blog posts',
 }
 
 const BlogPage = () => {
