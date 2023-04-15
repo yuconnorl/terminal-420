d383db86f0181e6eb661e728bf89bf8cbd433eda
yuconnorl <yuconnorl@gmail.com>
2023-04-15 23:31:02 +0800

    feat: add admonition

--- src/configs/general.ts
+++ src/configs/general.ts
@@ -1,1 +1,7 @@
 export const IS_DEV = process.env.NODE_ENV === 'development'
+
+export const ADMONITION_STYLES = {
+  info: 'bg-info/10 text-info border-info/50',
+  warn: 'bg-warn/10 text-warn border-warn/50',
+  danger: 'bg-danger/10 text-danger border-danger/50',
+}
