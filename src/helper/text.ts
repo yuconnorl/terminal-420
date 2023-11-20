e8f5fde707c784e7536594267087389f95ff9109
yuc <yuconnorl@gmail.com>
2023-11-20 17:18:02 +0800

    chore: update category psycheelic and cannabis

--- src/helper/text.ts
+++ src/helper/text.ts
@@ -1,5 +1,7 @@
 export function categoryTextFormatter (word: string){
-  if (/-/.test(word)) return word.split('-').map(w => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase()).join(' ');
+  if (/-/.test(word)) return word.split('-').map(w => {
+    if (w === 'and') return w;
+    return w.charAt(0).toUpperCase() + w.slice(1).toLowerCase()
+  }).join(' ');
   return word.charAt(0).toUpperCase() + word.slice(1)
 }
-        
