512370ab78bc2277b43a09506bdc96171946a309
yuc <yuconnorl@gmail.com>
2025-05-02 18:08:22 +0800

    feat: upgrade eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import prettier from 'eslint-plugin-prettier'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
+const eslintConfig = [
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'prettier', // Add prettier as an extension
+  ),
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx', '**/*.mjs'],
+    ignores: ['**/*.mdx', '**/*.md'],
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'arrow-body-style': 'off',
+      'prefer-arrow-callback': 'off',
+      'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]' }],
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+]
+
+export default eslintConfig
--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,3 @@
-const config = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
+export default {
+  plugins: ["@tailwindcss/postcss"],
 };
-
-module.exports = config;
