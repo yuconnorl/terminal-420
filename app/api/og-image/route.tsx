0c86a53ca1048600fbb9ce0a3731757a0467199c
yuc <yuconnorl@gmail.com>
2025-05-06 17:52:37 +0800

    fix: adjust font format to match max function limit

--- app/api/og-image/route.tsx
+++ app/api/og-image/route.tsx
@@ -1,8 +1,9 @@
+import fs from 'fs'
 import { ImageResponse } from 'next/og'
 import { NextRequest } from 'next/server'
 
 // Route segment config
-export const runtime = 'edge'
+export const runtime = 'nodejs'
 
 /** Generate OG image */
 export async function GET(request: NextRequest) {
@@ -11,19 +12,8 @@ export async function GET(request: NextRequest) {
     const hasTitle = searchParams.has('title')
     const title = hasTitle ? searchParams.get('title') : 'Not found'
 
-    const notoTcRegular = await fetch(new URL('public/fonts/noto-sans-tc-regular.ttf', import.meta.url))
-    const cubic_11 = await fetch(new URL('public/fonts/cubic_11.ttf', import.meta.url))
-
-    if (!notoTcRegular.ok) {
-      throw new Error('Failed to fetch the font file for og image')
-    }
-
-    if (!cubic_11.ok) {
-      throw new Error('Failed to fetch the font file for og image')
-    }
-
-    const notoTcRegularData = await notoTcRegular.arrayBuffer()
-    const cubic_11Data = await cubic_11.arrayBuffer()
+    const cubic_11 = fs.readFileSync('public/fonts/cubic_11.woff')
+    const cubic_11Data = cubic_11
 
     return new ImageResponse(
       (
@@ -35,15 +25,12 @@ export async function GET(request: NextRequest) {
             flexDirection: 'column',
             position: 'relative',
             backgroundColor: 'white',
+            fontFamily: 'Cubic 11',
           }}
         >
           <div tw='flex flex-col absolute bottom-20 left-20'>
-            <div tw='mb-12 text-[3.1rem] leading-[1.3] max-w-4xl' style={{ fontFamily: 'Cubic 11' }}>
-              {title}
-            </div>
-            <div tw='leading-tight text-base opacity-50' style={{ fontFamily: 'Noto Sans TC' }}>
-              Terminal 420
-            </div>
+            <div tw='mb-6 text-[3.1rem] leading-[1.3] max-w-4xl'>{title}</div>
+            <div tw='leading-tight text-lg opacity-50'>Terminal 420</div>
           </div>
         </div>
       ),
@@ -51,11 +38,6 @@ export async function GET(request: NextRequest) {
         width: 1200,
         height: 630,
         fonts: [
-          {
-            name: 'Noto Sans TC',
-            data: notoTcRegularData,
-            style: 'normal',
-          },
           {
             name: 'Cubic 11',
             data: cubic_11Data,
