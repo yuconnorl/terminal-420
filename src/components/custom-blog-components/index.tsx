b984480eaf6b4cce7b770cbd51e62da246db9a92
yuc <yuconnorl@gmail.com>
2025-05-07 08:55:58 +0800

    feat: update component for blog

--- src/components/custom-blog-components/index.tsx
+++ src/components/custom-blog-components/index.tsx
@@ -1,6 +1,7 @@
 import Admonition from './admonition'
 import Callout from './callout'
+import CustomAccordion from './custom-accordion'
 import CustomImage from './custom-image'
 import SpecialAdmonition from './special-admonition'
 
-export { Admonition, Callout, CustomImage, SpecialAdmonition }
+export { Admonition, Callout, CustomAccordion, CustomImage, SpecialAdmonition }
