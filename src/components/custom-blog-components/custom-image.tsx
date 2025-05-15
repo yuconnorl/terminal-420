5d796365dfcaad4373359afb9afafb8c8b77483b
yuc <yuconnorl@gmail.com>
2025-05-15 12:02:17 +0800

    feat: wip

--- src/components/custom-blog-components/custom-image.tsx
+++ src/components/custom-blog-components/custom-image.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Markdown from 'react-markdown'
 
 import { IMAGE_PLACEHOLDER_URL } from '@/configs/general'
 import { cn } from '@/lib/utils'
@@ -25,7 +26,26 @@ const CustomImage = ({ src, caption, alt, width, height }: Props) => {
         // unoptimized
       />
       {caption && (
-        <figcaption className='mt-2 pl-1 text-sm text-neutral-800 dark:text-neutral-300'>{caption}</figcaption>
+        <figcaption className='mt-2 pl-1 text-sm text-neutral-800 dark:text-neutral-300'>
+          <Markdown
+            components={{
+              a: ({ node, ...props }) => (
+                <span className='not-prose m-0 mr-[2px] text-[#8c796a] transition-opacity hover:opacity-70 dark:text-[#c3bbae]'>
+                  <a
+                    className='after:contents-[""] relative underline underline-offset-2 after:relative after:left-[2px] after:inline-block after:h-3 after:w-3 after:bg-[url(/images/link-arrow-dark.svg)] after:bg-contain after:bg-center after:bg-no-repeat dark:after:bg-[url(/images/link-arrow.svg)]'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    href={props.href}
+                  >
+                    {props.children}
+                  </a>
+                </span>
+              ),
+            }}
+          >
+            {caption}
+          </Markdown>
+        </figcaption>
       )}
     </figure>
   )
