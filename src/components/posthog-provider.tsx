3789849c6b0f4c61871db082e334eb5d8a92fb59
yuc <yuconnorl@gmail.com>
2025-10-11 09:26:26 +1000

    update posthog setting

--- src/components/posthog-provider.tsx
+++ src/components/posthog-provider.tsx
@@ -11,7 +11,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
-      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+      person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
       capture_pageview: false, // Disable automatic pageview capture, as we capture manually
     })
   }, [])
